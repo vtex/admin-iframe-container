a7aed10f76d3dad4df1fadca352f9db49819c3fb
kevin <kevinchevallier@gmail.com>
2021-05-18 20:30:52 -0300

    add attributes

--- react/components/Iframe.js
+++ react/components/Iframe.js
@@ -43,16 +43,30 @@ class Iframe extends Component {
     if (this.iframe) {
       this.updateChildLocale(this.context.culture.locale)
 
-      this.iframe.contentWindow.addEventListener('hashchange', this.handleIframeNavigation)
-      this.iframe.contentWindow.addEventListener('querychange', this.handleIframeNavigation)
-      this.iframe.contentWindow.addEventListener('popstate', this.handleIframeNavigation)
+      this.iframe.contentWindow.addEventListener(
+        'hashchange',
+        this.handleIframeNavigation
+      )
+      this.iframe.contentWindow.addEventListener(
+        'querychange',
+        this.handleIframeNavigation
+      )
+      this.iframe.contentWindow.addEventListener(
+        'popstate',
+        this.handleIframeNavigation
+      )
       const iframeHistory = this.iframe.contentWindow.history
       const handleIframeNavigation = this.handleIframeNavigation
-      if (iframeHistory
-        && iframeHistory.pushState !== iframeHistory.originalPushState) {
+      if (
+        iframeHistory &&
+        iframeHistory.pushState !== iframeHistory.originalPushState
+      ) {
         iframeHistory.originalPushState = iframeHistory.pushState
-        iframeHistory.pushState = function() {
-          const result = iframeHistory.originalPushState.apply(iframeHistory, arguments)
+        iframeHistory.pushState = function () {
+          const result = iframeHistory.originalPushState.apply(
+            iframeHistory,
+            arguments
+          )
           handleIframeNavigation()
           return result
         }
@@ -61,7 +75,7 @@ class Iframe extends Component {
     }
   }
 
-  updateIframeHistory = e => {
+  updateIframeHistory = (e) => {
     const { isDeloreanAdmin } = this.props
     const { pathname, search, hash } = window.location
     const {
@@ -72,38 +86,51 @@ class Iframe extends Component {
 
     const patchedIframeSearch = iframeSearch.replace(/(\?|\&)env\=beta/, '')
 
-    if (iframePathname.replace(isDeloreanAdmin ? '/iframe' : '/app', '') !== pathname
-      || (search !== patchedIframeSearch)
-      || (hash !== iframeHash)) {
-
-      const newPath = `${
-        pathname.replace('/admin', isDeloreanAdmin ? '/admin/iframe' : '/admin/app')
-      }${search}${hash}`
+    if (
+      iframePathname.replace(isDeloreanAdmin ? '/iframe' : '/app', '') !==
+        pathname ||
+      search !== patchedIframeSearch ||
+      hash !== iframeHash
+    ) {
+      const newPath = `${pathname.replace(
+        '/admin',
+        isDeloreanAdmin ? '/admin/iframe' : '/admin/app'
+      )}${search}${hash}`
 
       this.iframe.contentWindow.location.replace(newPath.replace(/\/+/g, '/'))
     }
   }
 
-  updateBrowserHistory = ({ pathname: iframePathname, search: iframeSearch, hash: iframeHash }) => {
+  updateBrowserHistory = ({
+    pathname: iframePathname,
+    search: iframeSearch,
+    hash: iframeHash,
+  }) => {
     const { isDeloreanAdmin } = this.props
     const { pathname, search = '', hash = '' } = window.location
 
     const patchedIframeSearch = iframeSearch.replace(/(\?|\&)env\=beta/, '')
 
-    let formatedIframePathname = iframePathname.replace(isDeloreanAdmin ? '/iframe' : '/app', '')
+    let formatedIframePathname = iframePathname.replace(
+      isDeloreanAdmin ? '/iframe' : '/app',
+      ''
+    )
 
     const formatedPathname = !pathname.endsWith('/') ? `${pathname}/` : pathname
-    formatedIframePathname = !formatedIframePathname.endsWith('/') ? `${formatedIframePathname}/` : formatedIframePathname
+    formatedIframePathname = !formatedIframePathname.endsWith('/')
+      ? `${formatedIframePathname}/`
+      : formatedIframePathname
 
-    if (formatedIframePathname !== formatedPathname
-      || (decodeURIComponent(search) !== decodeURIComponent(patchedIframeSearch))
-      || (decodeURIComponent(hash) !== decodeURIComponent(iframeHash))) {
-
+    if (
+      formatedIframePathname !== formatedPathname ||
+      decodeURIComponent(search) !== decodeURIComponent(patchedIframeSearch) ||
+      decodeURIComponent(hash) !== decodeURIComponent(iframeHash)
+    ) {
       const newPath = `${formatedIframePathname}${patchedIframeSearch}${iframeHash}`
 
       // here we use replaceState because the iframe navigation already pushed its url to the browser history, so we only need to
       // change the current browser url
-      window.history.replaceState({}, '', newPath);
+      window.history.replaceState({}, '', newPath)
     }
   }
 
@@ -112,19 +139,25 @@ class Iframe extends Component {
     const hash = loaded ? window.location.hash : ''
     const search = loaded ? window.location.search || '' : ''
     const env = getEnv()
-    const patchedSearch = env !== 'beta'
-      ? search
-      : search === ''
+    const patchedSearch =
+      env !== 'beta'
+        ? search
+        : search === ''
         ? '?env=beta'
         : search.includes('env=beta')
-          ? search
-          : search + '&env=beta'
-    return `/admin/${props.isDeloreanAdmin ? 'iframe' : 'app'}/${props.params.slug || ''}${patchedSearch || ''}${hash || ''}`
+        ? search
+        : search + '&env=beta'
+    return `/admin/${props.isDeloreanAdmin ? 'iframe' : 'app'}/${
+      props.params.slug || ''
+    }${patchedSearch || ''}${hash || ''}`
   }
 
   componentDidUpdate() {
     const nextSrc = this.buildSrc(this.props)
-    if (this.props.isDeloreanAdmin || !(this.iframe && this.iframe.contentWindow.location.pathname === nextSrc)){
+    if (
+      this.props.isDeloreanAdmin ||
+      !(this.iframe && this.iframe.contentWindow.location.pathname === nextSrc)
+    ) {
       this.iframe && this.iframe.contentWindow.location.replace(nextSrc)
     }
   }
@@ -132,22 +165,25 @@ class Iframe extends Component {
   fixedSrc = ''
 
   render() {
-    const { isDeloreanAdmin, params: { slug = '' } } = this.props
+    const {
+      isDeloreanAdmin,
+      params: { slug = '' },
+    } = this.props
     if (startsWith(isDeloreanAdmin ? 'iframe/' : 'app/', slug)) {
       return
     }
     const { loaded } = this.state
     const { customHeightGap } = this.props
     const style = {
-      width: "100%",
-      overflowY: "scroll",
+      width: '100%',
+      overflowY: 'scroll',
       // Admin V3's header height is 48px (3em), while the following version's
-      // header (Admin V4) is 56px (3.5em), and has a global alert that's displayed 
+      // header (Admin V4) is 56px (3.5em), and has a global alert that's displayed
       // on top of the header and has different heights depending on the
       // viewport. The customHeightGap prop is used here to fit the iframe
       // within the app in a flexible enough fashion, which allows to cover
       // multiple use cases from within the apps that use this app.
-      height: `calc(100vh - ${customHeightGap ? customHeightGap : '3em' })`
+      height: `calc(100vh - ${customHeightGap ? customHeightGap : '3em'})`,
     }
 
     // if we update the iframe src, the iframe redirects to the src address and it automatically propagates this navigation to
@@ -166,6 +202,7 @@ class Iframe extends Component {
         ref={this.handleRef}
         onLoad={this.handleOnLoad}
         data-hj-suppress
+        data-testid="admin-frame-container"
       />
     ) : null
   }
--- react/components/IframeLegacy.js
+++ react/components/IframeLegacy.js
@@ -168,6 +168,7 @@ class IframeLegacy extends Component {
           ref={this.handleRef}
           onLoad={this.handleOnLoad}
           data-hj-suppress
+          data-testid="admin-frame-container-legacy"
         />
       </div>
     ) : null
